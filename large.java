0a6a5aa38e2e4f3f0b11df1e03e119e6ef70efa4
aswhadhaaanbukkaman <aswhadhaa111@gmail.com>
2016-08-29 12:03:46 +0530

    Create large.java

---
+++ large.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class large
+{
+public static void main(String str)
+{
+Scanner sc=new Scanner(System.in);
+int[] p=new int[3];
+for(int i=0;i<3;i++)
+{
+p[i]=sc.nextInt();
+}
+Arrays.sort(p);
+int n=p.length;
+System.out.print(p[n-1)):
+}
+}
+
